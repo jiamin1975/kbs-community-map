1260ed585a432d67245a670daea000a73a229ca8
Liu <liujiamin@nih.gov>
2026-08-27 14:04:18 -0400

    3 step collapse design

--- components/library-map-experience.tsx
+++ components/library-map-experience.tsx
@@ -124,7 +124,7 @@ export function LibraryMapExperience() {
       >
         <DialogContent className="max-h-[90vh] overflow-y-auto rounded-none max-sm:left-2 max-sm:right-2 max-sm:top-2 max-sm:h-[calc(100dvh-1rem)] max-sm:max-h-[calc(100dvh-1rem)] max-sm:w-auto max-sm:max-w-none max-sm:translate-x-0 max-sm:translate-y-0 max-sm:p-3 sm:max-w-3xl">
           <DialogHeader>
-            <DialogTitle>Update Book Box Book List</DialogTitle>
+            <DialogTitle>Update Book Box</DialogTitle>
           </DialogHeader>
 
           {selectedLibrary && (
